ee406474fcf824da77bf934efb44afd3833c7677
Babarinde Abdulkabir <babarindeabdulkabiradio@gmail.com>
2022-09-13 15:20:03 -0700

    islower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print 1 and 0 depend on inputs
+ *
+ * Description: print all lower case
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-first 10 alphabet
+check for lower case character
